b79be35e1affc20ae3f0a18e6155553d2e2c62fd
John-507 <jambzz53@gmail.com>
2023-12-09 14:14:10 -0500

    Se creo metodo de peticion http a url del backend

--- src/app/servicio/api.service.ts
+++ src/app/servicio/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private url = 'http://localhost:5000/diagnosticar';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  diagnosticar(sintomas: string[]) {
+    // Este método hace la petición HTTP POST al backend
+    return this.http.post(this.url, { sintomas });
+  }
 }
