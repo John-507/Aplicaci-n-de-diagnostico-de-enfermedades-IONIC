ef07468838e22495e7be93a218433a1e893e870a
John-507 <jambzz53@gmail.com>
2023-12-09 22:01:15 -0500

    Se creo metodo para obtener lista de sintomas del backend

--- src/app/servicio/api.service.ts
+++ src/app/servicio/api.service.ts
@@ -5,12 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiService {
-  private url = 'http://localhost:5000/diagnosticar';
+  private urlDiagnostico = 'http://localhost:5000/diagnosticar';
+  private urlSintomas = 'http://localhost:5000/sintomas';
 
   constructor(private http: HttpClient) { }
 
   diagnosticar(sintomas: string[]) {
     // Este método hace la petición HTTP POST al backend
-    return this.http.post(this.url, { sintomas });
+    return this.http.post(this.urlDiagnostico, { sintomas });
   }
+
+  obtenerSintomas() {
+    return this.http.get<string[]>(this.urlSintomas);
+  }
+
+  
 }
