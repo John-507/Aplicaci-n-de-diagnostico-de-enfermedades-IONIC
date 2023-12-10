69901371219d8ab9651dbd47fa9e0718528a340e
John-507 <jambzz53@gmail.com>
2023-12-09 22:31:32 -0500

    Se creo funciones cargarSintomas, cambiarSeleccion y enviar en pagina sintomas

--- src/app/sintomas/sintomas.page.ts
+++ src/app/sintomas/sintomas.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../servicio/api.service';
+import { LoadingController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sintomas',
@@ -8,9 +9,71 @@ import { ApiService } from './../servicio/api.service';
 })
 export class SintomasPage implements OnInit {
 
-  constructor() { }
+  sintomas: any[] = [];
+
+  seleccionados: any = {};
+
+
+  constructor(
+    private api:ApiService,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
   }
 
+  async cargarSintomas(){
+    // Muestra un indicador de carga mientras se obtienen los síntomas
+    const loading = await this.loadingCtrl.create({
+      message: 'Cargando síntomas...'
+    });
+    await loading.present();
+
+    // llama al método del servicio para obtener los síntomas
+    this.api.obtenerSintomas().subscribe(sintomas => {
+      // Asigna los síntomas obtenidos al array 'sintomas'
+      this.sintomas = sintomas;
+      //oculta el indicador de carga
+      loading.dismiss();
+    }, err => {
+      // Maneja el error si existe
+      console.error('Error al cargar los sintomas:', err);
+      loading.dismiss();
+    });
+  }
+
+  cambiarSeleccion(sintoma: string, valor: boolean) {
+    // Asigna el valor al objeto 'seleccionados'
+    this.seleccionados[sintoma] = valor;
+  }
+
+  async enviar() {
+    // Mostrar un indicador de carga
+    const loading = await this.loadingCtrl.create({
+      message: 'Enviando datos...'
+    });
+    await loading.present();
+
+    // Obtiene los síntomas seleccionados como un array
+    const sintomasSeleccionados = Object.keys(this.seleccionados).filter(key => this.seleccionados[key]);
+
+    // Llama al método diagnosticar del servicio ApiService
+    this.api.diagnosticar(sintomasSeleccionados).subscribe(res => {
+      // Oculta el indicador de carga
+      loading.dismiss();
+
+      // Navega a la página de resultado pasando los datos como parámetros
+      this.navCtrl.navigateForward(['/resultado'], {
+        queryParams: { diagnostico: JSON.stringify(res) }
+      });
+    }, err => {
+      // Maneja el error aquí si ocurre alguno
+      console.error('Error al enviar los síntomas:', err);
+      loading.dismiss();
+    });
+  }
+
+
+  
 }
