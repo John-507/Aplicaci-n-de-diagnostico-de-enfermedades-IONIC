ebe8da03b2dfdd939778a0d2b04e5cfc44d63f4d
John-507 <jambzz53@gmail.com>
2023-12-10 14:53:45 -0500

    Se depura codigo mediante consola

--- src/app/sintomas/sintomas.page.ts
+++ src/app/sintomas/sintomas.page.ts
@@ -15,7 +15,7 @@ export class SintomasPage implements OnInit {
 
 
   constructor(
-    private api:ApiService,
+    private api: ApiService,
     private loadingCtrl: LoadingController,
     private navCtrl: NavController
   ) { }
@@ -24,7 +24,7 @@ export class SintomasPage implements OnInit {
     this.cargarSintomas();
   }
 
-  async cargarSintomas(){
+  async cargarSintomas() {
     // Muestra un indicador de carga mientras se obtienen los síntomas
     const loading = await this.loadingCtrl.create({
       message: 'Cargando síntomas...'
@@ -35,6 +35,7 @@ export class SintomasPage implements OnInit {
     this.api.obtenerSintomas().subscribe(sintomas => {
       // Asigna los síntomas obtenidos al array 'sintomas'
       this.sintomas = sintomas;
+      console.log('Lista de síntomas:', this.sintomas);
       //oculta el indicador de carga
       loading.dismiss();
     }, err => {
@@ -45,8 +46,17 @@ export class SintomasPage implements OnInit {
   }
 
   cambiarSeleccion(sintoma: string, valor: boolean) {
-    // Asigna el valor al objeto 'seleccionados'
-    this.seleccionados[sintoma] = valor;
+    // Convierte el ID de sintoma a un número
+    const idNumerico = Number(sintoma);
+    this.seleccionados[idNumerico] = valor;
+
+    // Encuentra el objeto de síntoma basado en el ID numérico
+    let sintomaObjeto = this.sintomas.find(sintoma => sintoma.id === idNumerico);
+    if (sintomaObjeto && sintomaObjeto.nombre) {
+      console.log(`ID: ${idNumerico}, Nombre: ${sintomaObjeto.nombre}, Seleccionado: ${valor}`);
+    } else {
+      console.log(`No se encontró un nombre para el ID: ${idNumerico}`);
+    }
   }
 
   async enviar() {
@@ -58,6 +68,9 @@ export class SintomasPage implements OnInit {
 
     // Obtiene los síntomas seleccionados como un array
     const sintomasSeleccionados = Object.keys(this.seleccionados).filter(key => this.seleccionados[key]);
+
+    // Muestra los síntomas seleccionados en la consola
+    console.log('Síntomas seleccionados:', sintomasSeleccionados);
 
     // Llama al método diagnosticar del servicio ApiService
     this.api.diagnosticar(sintomasSeleccionados).subscribe(res => {
