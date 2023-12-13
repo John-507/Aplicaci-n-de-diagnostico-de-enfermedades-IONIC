528a59d6959cb25d76d7d1a3c3c5a9d93eb060ed
John-507 <jambzz53@gmail.com>
2023-12-13 00:51:16 -0500

    Se modifica el servicio para utilizar ngrok y se arregla problema con boton enviar

--- src/app/servicio/api.service.ts
+++ src/app/servicio/api.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private urlDiagnostico = 'http://192.168.1.109:5000/diagnosticar';
-  private urlSintomas = 'http://192.168.1.109:5000/sintomas';
+  private urlDiagnostico = 'https://7c7c-190-218-34-61.ngrok-free.app/diagnosticar';
+  private urlSintomas = 'https://7c7c-190-218-34-61.ngrok-free.app/sintomas';
 
   constructor(private http: HttpClient) { }
 
   diagnosticar(sintomas: string[]) {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'ngrok-skip-browser-warning': 'true' 
+      })
+    };
     // Este método hace la petición HTTP POST al backend
-    return this.http.post(this.urlDiagnostico, { sintomas });
+    return this.http.post(this.urlDiagnostico, { sintomas },httpOptions);
   }
 
   obtenerSintomas() {
-    return this.http.get<any[]>(this.urlSintomas);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'ngrok-skip-browser-warning': 'true' 
+      })
+    };
+    return this.http.get<any[]>(this.urlSintomas,httpOptions);
   }
 
   
